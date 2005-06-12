cdeb20f217659c8c5ff3b34c6dc83fdc2143d466
Kevin Reid <kpreid@switchb.org>
2005-06-12 15:43:32 +0000

    New 'fd-ref' object which provides the posix read interface for sockets
    git-svn-id: http://switchb.org/svn/e/cl-e/trunk@95 bf3ccfa1-f3f3-0310-a3d5-cea1fe9d5a75

--- lisp/sockets.lisp
+++ lisp/sockets.lisp
@@ -37,32 +37,39 @@
   (:|getSockoptReceiveBuffer/0| #'sockopt-receive-buffer)
   (:|setSockoptReceiveBuffer/1| (this new) (setf (sockopt-receive-buffer this) new))
   
-  ; XXX this should be on a general fd-ref object
-  ; XXX be able to avoid allocating the buffer
   (:|read/3| (this max-octets error-ejector eof-ejector)
     "Read up to 'max-octets' currently available octets from the socket, and return them as a ConstList."
-    ; thanks to nyef on irc://irc.freenode.net/lisp for this code --
-    ;   http://paste.lisp.org/display/7891
-    ;   http://meme.b9.com/cview.html?utime=3324058742&channel=lisp&start=3324055147&end=3324062347#utime_requested
-    (let ((buf (make-array max-octets :element-type '(unsigned-byte 8)
-                                      :fill-pointer 0
-                                      :adjustable nil)))
-      (multiple-value-bind (n-read errno)
-          ; XXX SBCL internal package. Show me something more appropriate and I'll use it
-          (sb-unix:unix-read (socket-file-descriptor this)
-                             (sb-sys:vector-sap (sb-impl::%array-data-vector buf))
-                             max-octets)
-        (case n-read
-          ((nil)
-            (case errno
-              ((#.sb-unix:ewouldblock)
-                buf)
-              (otherwise
-                ; XXX typed error with errno slot
-                (eject-or-ethrow error-ejector (format nil "file descriptor read error: ~A (~A)" errno (sb-int:strerror errno))))))
-          ((0)
-            (eject-or-ethrow eof-ejector "socket EOF"))
-          (otherwise
-            (setf (fill-pointer buf) n-read)
-            buf)))))
+    (e. (make-fd-ref (socket-file-descriptor this)) |read| max-octets error-ejector eof-ejector))
   )
+  
+
+; XXX this should not be in sockets but in something more general
+(defun make-fd-ref (fd)
+  (e-named-lambda "org.cubik.cle.io.FDRef"
+    (:|read| (max-octets error-ejector eof-ejector)
+    "Read up to 'max-octets' currently available octets from the FD, and return them as a ConstList. Blocks if read(2) would block."
+      ; XXX be able to avoid allocating the buffer
+      ; thanks to nyef on irc://irc.freenode.net/lisp for this code --
+      ;   http://paste.lisp.org/display/7891
+      ;   http://meme.b9.com/cview.html?utime=3324058742&channel=lisp&start=3324055147&end=3324062347#utime_requested
+      (let ((buf (make-array max-octets :element-type '(unsigned-byte 8)
+                                        :fill-pointer 0
+                                        :adjustable nil)))
+        (multiple-value-bind (n-read errno)
+            ; XXX SBCL internal package. Show me something more appropriate and I'll use it
+            (sb-unix:unix-read fd
+                               (sb-sys:vector-sap (sb-impl::%array-data-vector buf))
+                               max-octets)
+          (case n-read
+            ((nil)
+              (case errno
+                ((#.sb-unix:ewouldblock)
+                  buf)
+                (otherwise
+                  ; XXX typed error with errno slot
+                  (eject-or-ethrow error-ejector (format nil "file descriptor read error: ~A (~A)" errno (sb-int:strerror errno))))))
+            ((0)
+              (eject-or-ethrow eof-ejector "socket EOF"))
+            (otherwise
+              (setf (fill-pointer buf) n-read)
+              buf)))))))
