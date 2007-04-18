4d85f65cc15330ff3c0b4f6a19705439f30ac265
Kevin Reid <kpreid@switchb.org>
2007-04-18 16:34:02 +0000

    Remove duplication in cleaning scripts; reformatting; make antlr/clean.sh executable.
    git-svn-id: http://switchb.org/svn/e/cl-e/trunk@770 bf3ccfa1-f3f3-0310-a3d5-cea1fe9d5a75

--- antlr/clean.sh
+++ antlr/clean.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
-rm -f *.class
-rm -f {E,EALexer,QuasiLexer}TokenTypes.{txt,java}
-rm -f {EALexer,QuasiLexer,EParser}.java
+
+# Copyright 2006-2007 Kevin Reid, under the terms of the MIT X license
+# found at http://www.opensource.org/licenses/mit-license.html ................
+
+set -e
+
+rm -f *.class \
+      {E,EALexer,QuasiLexer}TokenTypes.{txt,java} \
+      {EALexer,QuasiLexer,EParser}.java
