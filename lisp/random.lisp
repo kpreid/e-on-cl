0ee6f961c09e1575e9d37a82a634129565e77acc
Kevin Reid <kpreid@switchb.org>
2009-07-31 03:06:59 +0000

    Implemented Set#sort/0 and entropy.nextInt/1. entropy.nextSwiss/0 is now defined in terms of random-state#nextInt/1 instead of the Swiss size definition being in CL code.
    git-svn-id: http://switchb.org/svn/e/cl-e/trunk@1049 bf3ccfa1-f3f3-0310-a3d5-cea1fe9d5a75

--- lisp/random.lisp
+++ lisp/random.lisp
@@ -3,14 +3,13 @@
 
 (in-package :e.elib)
 
-(defconstant +swiss-size-in-octets+ 20)
-(defconstant +swiss-upper-bound+ (expt 2 (* 8 +swiss-size-in-octets+)))
-
 (def-vtable random-state
   (:|__printOn| (this (out +the-text-writer-guard+))
     (with-standard-io-syntax
       (e. out |write| (prin1-to-string this))))
   (:|nextSwiss| (this)
     (random +swiss-upper-bound+ this))
+  (:|nextInt| (this range)
+    (random range this))
   (:|diverge| (this)
     (make-random-state this)))
